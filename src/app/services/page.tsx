8253632957013216d751c4c7a544fec4d1c87312
Bryan Fernando Dinata <bfernando@student.ciputra.ac.id>
2026-01-11 20:58:12 +0700

    ci / cd test

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -2,8 +2,7 @@ import React from 'react'
 
 export default function page() {
     return (
-        <main className="overflow-hidden flex flex-col gap-24">
-
+        <main className="overflow-hidden min-h-screen flex flex-col gap-24">
         </main>
     )
 }
